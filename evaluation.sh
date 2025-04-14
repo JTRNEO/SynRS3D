3f26e016ad5aba51edbd65c089a5eedae28c3cec
jtrneo <songjian@g.ecc.u-tokyo.ac.jp>
2025-04-14 12:49:09 +0900

    Update evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -5,6 +5,6 @@ python evaluation.py \
 --test_datasets DFC18 DFC19_JAX  \
 --ood_datasets DFC19_JAX \
 --multi_task \
---images_file train.txt test_syn.txt test_syn.txt \
+--images_file train.txt test.txt test.txt \
 --save_num_images 0 \
 --snapshot_dir /result
